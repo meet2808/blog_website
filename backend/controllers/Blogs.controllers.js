01c36b492ad86acaa6197bbef974be72fd6b8f4a
Meet Panchal <meetpanchal2803@gmail.com>
2024-03-21 18:33:56 +0530

    Add other routes in blog routes

--- backend/controllers/Blogs.controllers.js
+++ backend/controllers/Blogs.controllers.js
@@ -7,7 +7,6 @@ export const createBlog = async (req, res) => {
     try {
         const fields = ["title", "category", "content"];
         const emptyFields = fields.filter(field => !req.body[field]);
-        console.log(emptyFields.length, emptyFields)
 
         if (emptyFields.length !== 0) return res.status(403).json({ error: "Please fill in all the required fields." });
 
@@ -33,6 +32,26 @@ export const createBlog = async (req, res) => {
     }
 }
 
+export const updateBlog = async (req, res) => {
+    const errors = validationResult(req);
+    try {
+        const fields = ["title", "category", "content"];
+        const emptyFields = fields.filter(field => !req.body[field]);
+
+        if (emptyFields.length !== 0) return res.status(403).json({ error: "Please fill in all the required fields." });
+
+        if (!errors.isEmpty()) {
+            return res.status(403).json(errors);
+        } else {
+            let updatedBlog = await Blog.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+            res.status(200).json(updatedBlog);
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({ message: "Error occured while updating the blog" });
+    }
+}
+
 export const getAllBlogs = async (req, res) => {
     try {
         let blogs = await Blog.find();
@@ -46,9 +65,40 @@ export const getAllBlogs = async (req, res) => {
 export const getBlog = async (req, res) => {
     try {
         let blog = await Blog.findById(req.params.id);
-        res.status(200).send(blog)
+        res.status(200).send(blog);
     } catch (error) {
-        res.status(401).json({ message: error })
+        res.status(401).json({ message: error });
+    }
+}
+
+export const getBlogByCategory = async (req, res) => {
+    try {
+        let blogs = [];
+        blogs = await Blog.find({ category: req.params.category });
+        if (blogs.length !== 0 && blogs.length > 0) {
+            res.status(200).send(blogs);
+        } else if (blogs.length == 0) {
+            res.status(404).send({ message: "No blog found with this category." })
+        }
+    } catch (error) {
+        res.status(401).json({ message: error });
+    }
+}
+
+export const getBlogByAuthor = async (req, res) => {
+    try {
+        if (req.params.id === req.id) {
+            let blogs = [];
+            blogs = await Blog.find({ author_id: req.params.id });
+            if (blogs.length !== 0 && blogs.length > 0) {
+                res.status(200).send(blogs);
+            } else if (blogs.length == 0) {
+                res.status(404).send({ message: "No blog found with this author id." })
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({ message: error });
     }
 }
 
@@ -56,10 +106,10 @@ export const deleteBlog = async (req, res) => {
     try {
         let blog = await Blog.findByIdAndDelete(req.params.id);
         console.log(blog)
-        if(blog !== null)
-            res.status(200).send({ message : "Blog deleted successfully"})
-        else if(blog == null)
-            res.status(401).send({ message : "Blog not found or deleted,"})
+        if (blog !== null)
+            res.status(200).send({ message: "Blog deleted successfully" })
+        else if (blog == null)
+            res.status(401).send({ message: "Blog not found or deleted," })
     } catch (error) {
         res.status(401).json({ message: error })
     }
--- backend/routes/Blog.routes.js
+++ backend/routes/Blog.routes.js
@@ -1,6 +1,14 @@
 import express from "express";
 import { body } from "express-validator";
-import { createBlog, getAllBlogs, getBlog, deleteBlog } from "../controllers/Blogs.controllers.js";
+import {
+    createBlog,
+    getAllBlogs,
+    getBlog,
+    deleteBlog,
+    getBlogByCategory,
+    getBlogByAuthor,
+    updateBlog
+} from "../controllers/Blogs.controllers.js";
 import { verifyToken } from "../utils/auth.middleware.js";
 import { upload } from "../utils/multer.middleware.js";
 
@@ -13,10 +21,21 @@ router.route("/create").post(verifyToken, [
     body("image", "Please upload the image")
 ], upload.single('image'), createBlog);
 
+router.route("/update/:id").patch(verifyToken, [
+    body("title", "Please enter the title"),
+    body("category", "Please select the category"),
+    body("content", "Please enter the content"),
+    body("image", "Please upload the image")
+], upload.single('image'), updateBlog);
+
 router.route("/all").get(verifyToken, getAllBlogs)
 
 router.route("/:id").get(verifyToken, getBlog)
 
-router.route("/delete/:id").delete(verifyToken, deleteBlog)
+router.route("/delete/:id").delete(verifyToken, deleteBlog);
+
+router.route("/category/:category").get(verifyToken, getBlogByCategory);
+
+router.route("/author/:id").get(verifyToken, getBlogByAuthor)
 
 export default router;
